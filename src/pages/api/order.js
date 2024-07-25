d135ea7b7d5b078edb9c7705762b25b393734aa0
thehkmalhotra <96623687+thehkmalhotra@users.noreply.github.com>
2024-07-25 05:31:56 +0530

    Update order.js

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -10,8 +10,8 @@ export default async function order(req, res) {
 
         // Step 2: Prepare order details object
         const orderDetails = {
-            "payment_method": "cashfree",
-            "payment_method_title": "Cashfree",
+            "payment_method": "razorpay",
+            "payment_method_title": "Razorpay",
             "set_paid": true,
             "billing": {
                 "first_name": paymentDetails.fullName.split(" ")[0],
@@ -49,42 +49,6 @@ export default async function order(req, res) {
                 }
             ]
         };
-
-        // Step 3: Make API call to create the order
-        const key = "ck_49e1bda9eb68018ca0521c6f89d4d6eb05302a4f";
-        const secret = "cs_54ac72ff68ce4fa700fb4eba53ee5303fbfe1b61";
-
-        const orderResponse = await fetch("https://admin.gulfflora.ae/wp-json/wc/v3/orders", {
-            method: "POST",
-            headers: {
-                "Authorization": "Basic " + btoa(`${key}:${secret}`),
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(orderDetails)
-        });
-
-        const order = await orderResponse.json();
-
-        if (order.id) {
-            const cashfreeSessionResponse = await fetch("http://gulfflora.ae/api/cashfree/order", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    amount: order.total,
-                    currency: "INR",
-                    customer_name: `${order.billing.first_name} ${order.billing.last_name}`,
-                    customer_email: order.billing.email,
-                    customer_phone: order.billing.phone
-                })
-            });
-
-            const cashfreeSession = await cashfreeSessionResponse.json();
-            res.status(200).json({ cashfreeSession });
-        } else {
-            res.status(400).json({ error: "Something went wrong while creating the order." });
-        }
     } catch (error) {
         res.status(500).json({ error: "Failed to create order" });
     }
